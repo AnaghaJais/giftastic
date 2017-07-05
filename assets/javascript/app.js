c144e775a163fb914353ceaac078d1ec23397ded
AnaghaJais <anagha.babu1@gmail.com>
2017-07-04 19:09:55 -0500

    modified app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ function click(){
 $(document).on("click","button",function(){
 	var bird = $(this).attr("data");
 	console.log(bird);
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         bird + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       $.ajax({
